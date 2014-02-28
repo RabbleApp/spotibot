8a7e2df93b9c0808e4c43ac895960716c12ba9b1
Oscar <onilsson@outlook.com>
2014-02-28 16:13:43 +0100

    Prettify tests

--- spec/spotify_queue_spec.rb
+++ spec/spotify_queue_spec.rb
@@ -4,12 +4,15 @@ describe SpotifyQueue do
   describe "#enqueue" do
     it "adds the song at the end of the queue" do
       queue = SpotifyQueue.new
+
       queue.enqueue("moo")
       queue.enqueue("foo")
+
       last_song = queue.queue.pop
       expect(last_song).to eq("foo")
     end
   end
+
   describe "#dequeue" do
     it "removes the specified item from the queue" do
       queue = SpotifyQueue.new
@@ -19,6 +22,7 @@ describe SpotifyQueue do
       queue.enqueue("big o & hassan")
 
       queue.dequeue("katy perry")
+
       expect(queue.queue).to_not include("katy perry")
     end
   end
